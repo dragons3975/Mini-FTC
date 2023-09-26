f97abd1a6a85cc610549c3de15865da47b3f01f6
ylchevoir <yann.lechevoir@cae.com>
2023-09-26 17:31:35 -0400

    Bras bouge

--- src/main/java/ev3/robot/RobotContainer.java
+++ src/main/java/ev3/robot/RobotContainer.java
@@ -1,10 +1,13 @@
 package ev3.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import ev3.robot.Constants.OIConstants;
 import ev3.robot.commandGroups.AutonomousCommandGroup;
 import ev3.robot.commands.DriveCommand;
+import ev3.robot.commands.TestBrasCommand;
 import ev3.robot.subsystems.BrasSubsystem;
 import ev3.robot.subsystems.DriveSubsystem;
 
@@ -14,6 +17,7 @@ public class RobotContainer {
 
     private final DriveSubsystem mDriveSubsystem = new DriveSubsystem();
     private final BrasSubsystem mBrasSubsystem = new BrasSubsystem();
+    private final TestBrasCommand mTestBrasCommand = new TestBrasCommand(mBrasSubsystem);
     private final DriveCommand mDriveCommand = new DriveCommand(mDriveSubsystem, mXboxController);
 
     private final AutonomousCommandGroup mAutonomousCommandGroup;
@@ -27,8 +31,8 @@ public class RobotContainer {
     }
 
     private void configureButtonBindings() {
-        //JoystickButton buttonX = new JoystickButton(mXboxController, Button.kA.value);
-        //buttonX.whileTrue(mDriveCommand);
+        JoystickButton buttonA = new JoystickButton(mXboxController, Button.kA.value);
+        buttonA.whileTrue(mTestBrasCommand);
     }
 
     private void configureDefaultCommands() {
--- src/main/java/ev3/robot/subsystems/BrasSubsystem.java
+++ src/main/java/ev3/robot/subsystems/BrasSubsystem.java
@@ -1,32 +1,26 @@
 package ev3.robot.subsystems;
 
-import dragons.ev3.ArduinoMotor;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 
 public class BrasSubsystem extends Subsystem {
 
-    private final ArduinoMotor m_motorBras1 = new ArduinoMotor(0);
-    private final ArduinoMotor m_motorBras2 = new ArduinoMotor(1);
-    private final ArduinoMotor m_motorBras3 = new ArduinoMotor(2);
-    private final ArduinoMotor m_motorBras4 = new ArduinoMotor(3);
+    private EV3LargeRegulatedMotor mMotorBras = new EV3LargeRegulatedMotor(MotorPort.C);
 
     public BrasSubsystem() {
     }
 
     @Override
     public void periodic() {
-        /*m_motorBras1.set(0.1);
-        m_motorBras2.set(0.2);
-        m_motorBras3.set(0.3);
-        m_motorBras4.set(0.4);
-        m_motorBras1.get();
-        m_motorBras2.get();
-        m_motorBras3.get();
-        m_motorBras4.get();
-        System.out.println(m_motorBras1.getTachoCount());
-        m_motorBras2.getTachoCount();
-        m_motorBras3.getTachoCount();
-        m_motorBras4.getTachoCount();*/
+    }
+
+    public void SatineBouge() { 
+        mMotorBras.set(1.0);
+    }
+
+    public void stop() {
+        mMotorBras.set(0);
     }
 
 }
