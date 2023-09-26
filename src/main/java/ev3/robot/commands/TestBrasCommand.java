35ef74d6ec3d27aa5e71fa98ec0b0590bb43d2a8
ylchevoir <yann.lechevoir@cae.com>
2023-09-26 18:37:13 -0400

    j'ai fait tourner quelque chose!!!

---
+++ src/main/java/ev3/robot/commands/TestBrasCommand.java
@@ -0,0 +1,37 @@
+package ev3.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import ev3.robot.subsystems.BrasSubsystem;
+
+public class TestBrasCommand extends Command {
+
+    private BrasSubsystem mBrasSubsystem;
+    
+    public TestBrasCommand(BrasSubsystem brasSubsystem) {
+        mBrasSubsystem = brasSubsystem;
+        addRequirements(brasSubsystem);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        mBrasSubsystem.mars(1.0);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        mBrasSubsystem.stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
