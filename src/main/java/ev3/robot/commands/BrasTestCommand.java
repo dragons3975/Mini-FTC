b52b16b807c5051a708676e4373a275d989343ed
dragons3975 <lesdragons3975@gmail.com>
2023-10-06 15:35:24 -0400

    Ajout capteur tactile

--- src/main/java/ev3/robot/commands/BrasTestCommand.java
+++ src/main/java/ev3/robot/commands/BrasTestCommand.java
@@ -34,6 +34,6 @@ public class BrasTestCommand extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return mBrasSubsystem.isCapteurEnfonce();
     }
 }
--- src/main/java/ev3/robot/subsystems/BrasSubsystem.java
+++ src/main/java/ev3/robot/subsystems/BrasSubsystem.java
@@ -2,13 +2,20 @@ package ev3.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import ev3dev.sensors.ev3.EV3TouchSensor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
 
 public class BrasSubsystem extends Subsystem {
 
     private final EV3LargeRegulatedMotor m_brasmotor = new EV3LargeRegulatedMotor(MotorPort.C);
-
+    private final EV3TouchSensor m_capteur = new EV3TouchSensor(SensorPort.S1);
+    
     public BrasSubsystem() {
+    }
+
+    public boolean isCapteurEnfonce(){
+        return m_capteur.isPressed();
     }
 
     @Override
