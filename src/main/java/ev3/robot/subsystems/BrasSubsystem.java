c4b5a4ca726ba09fa17bf3b3096b9967be7d0e12
ylchevoir <yann.lechevoir@cae.com>
2023-10-10 12:29:32 -0400

    Changed for MediumRegulatedMotor

--- src/main/java/ev3/robot/subsystems/BrasSubsystem.java
+++ src/main/java/ev3/robot/subsystems/BrasSubsystem.java
@@ -1,12 +1,12 @@
 package ev3.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import ev3dev.actuators.lego.motors.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
 public class BrasSubsystem extends Subsystem {
     
-    private final EV3LargeRegulatedMotor m_moteurPince = new EV3LargeRegulatedMotor(MotorPort.C);
+    private final EV3MediumRegulatedMotor m_moteurPince = new EV3MediumRegulatedMotor(MotorPort.C);
 
     public BrasSubsystem() {
     }
