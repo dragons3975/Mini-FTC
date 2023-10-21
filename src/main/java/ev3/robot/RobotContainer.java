298c3b4741783cbbb1b77f8a01819657dc4142bb
dragons3975 <35250302+dragons3975@users.noreply.github.com>
2023-10-21 15:01:27 -0400

    auto bases

--- src/main/java/ev3/robot/RobotContainer.java
+++ src/main/java/ev3/robot/RobotContainer.java
@@ -5,13 +5,17 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import ev3.robot.Constants.OIConstants;
+import ev3.robot.commandGroups.AutoCommandg1;
+import ev3.robot.commandGroups.AutoCommandg2;
 import ev3.robot.commandGroups.AutonomousCommandGroup;
 import ev3.robot.commands.BrasTestCommand;
 import ev3.robot.commands.DriveCommand;
 import ev3.robot.commands.TogglePinceCommand;
 import ev3.robot.subsystems.BrasSubsystem;
+import ev3.robot.subsystems.ColorSubsystem;
 import ev3.robot.subsystems.DriveSubsystem;
 import ev3.robot.subsystems.PinceSubsystem;
+import lejos.robotics.Color;
 
 public class RobotContainer {
 
@@ -19,12 +23,16 @@ public class RobotContainer {
 
     private final DriveSubsystem mDriveSubsystem = new DriveSubsystem();
     private final BrasSubsystem mBrasSubsystem = new BrasSubsystem();
+    private final ColorSubsystem mColorSubsystem = new ColorSubsystem();
     private final PinceSubsystem mPinceSubsystem = new PinceSubsystem();
     private final DriveCommand mDriveCommand = new DriveCommand(mDriveSubsystem, mXboxController);
     private final BrasTestCommand mBrasTestCommand = new BrasTestCommand(mBrasSubsystem);
     private final TogglePinceCommand mTogglePinceCommand = new TogglePinceCommand(mPinceSubsystem);
+    
 
     private final AutonomousCommandGroup mAutonomousCommandGroup;
+    private final AutoCommandg1 mAutoCommandg1;
+    private final AutoCommandg2 mAutoCommandg2;
 
     public RobotContainer() {
 
@@ -48,6 +56,19 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return mAutonomousCommandGroup;
+        if (mColorSubsystem.getcolor() == Color.RED)
+        {
+            return mAutonomousCommandGroup;
+        }
+        if (mColorSubsystem.getcolor() == Color.BLUE)
+        {
+            return mAutoCommandg1;
+        }
+        if (mColorSubsystem.getcolor() == Color.GREEN)
+        {
+            return mAutoCommandg2;        
+        }
+        
+        return null;
     }
 }
--- src/main/java/ev3/robot/subsystems/BrasSubsystem.java
+++ src/main/java/ev3/robot/subsystems/BrasSubsystem.java
@@ -3,9 +3,9 @@ package ev3.robot.subsystems;
 import edu.wpi.first.hal.DriverStationJNI.Telemetry;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
-import ev3dev.sensors.ev3.EV3TouchSensor;
+//import ev3dev.sensors.ev3.EV3TouchSensor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
+//import lejos.hardware.port.SensorPort;
 
 public class BrasSubsystem extends Subsystem {
 
